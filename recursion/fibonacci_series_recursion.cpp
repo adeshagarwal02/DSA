fa7e5cc4d49bcabc511d37b3369586b3990283a0
adesh <adesh@LAPTOP-R590QC50.>
2026-05-07 12:02:38 +0000

    Excerssive Recursion

---
+++ recursion/fibonacci_series_recursion.cpp
@@ -0,0 +1,24 @@
+// Excerssive Recurssion
+
+#include <iostream>
+
+
+int fibonacci(int n){
+
+	if(n <= 1){
+		return n;
+	}
+	return fibonacci(n-2) + fibonacci(n-1);
+}
+
+
+int main(void){
+
+	int n;
+
+	std::cout<<"enter the value of N:";
+	std::cin>>n;
+	std::cout<<fibonacci(n)<<std::endl;
+	return 0;
+}
+
