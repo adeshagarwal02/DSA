2a0275234aae8b8387e821077173a5c5ab1b0ed0
adesh <adesh@LAPTOP-R590QC50.>
2026-05-07 12:00:15 +0000

    Using memoization

---
+++ recursion/fibonacci_series_recursion_memoization.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <array>
+
+std::array<int,10> f;
+
+int fibonacci(int n)
+{
+	if(n <= 1){
+		f[n] = n;
+		return n;
+	}
+	else {
+		if(f[n-2] == -1){
+			f[n-2] = fibonacci(n-2);
+		}
+		if(f[n-1] == -1){
+			f[n-1] = fibonacci(n-1);
+		}
+	}
+	return f[n-2] + f[n-1]; 
+}
+
+int main(void){
+
+	f.fill(-1);
+	std::cout<<fibonacci(7)<<std::endl;
+	return 0;
+}
